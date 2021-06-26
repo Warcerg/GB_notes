32f43c21f706920bda787548a172951994150385
warcerg <warcerg@gmail.com>
2021-06-26 22:16:04 +0300

    Задание 7 сделано.
    Есть проблемы с горизонтальной ориентацией, а именно отображением фрагмента с листом. Подозреваю что из-за текущей логике по вызову фрагмента с детализацией заметки из фрагмента со списком, тогда же как остальный фрагменты вызываются напрямую из MainAcitivity

--- app/src/main/java/com/example/gb_notes/MainActivity.java
+++ app/src/main/java/com/example/gb_notes/MainActivity.java
@@ -1,15 +1,140 @@
 package com.example.gb_notes;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TOAST = "App info";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initview();
     }
+
+    private void initview() {
+        Toolbar toolbar = initToolbar();
+        initDrawer(toolbar);
+    }
+
+    private Toolbar initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        return toolbar;
+    }
+
+    private void initDrawer(Toolbar toolbar) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (navigateFragment(id)){
+                    drawer.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private boolean navigateFragment(int id) {
+        switch (id) {
+            case R.id.action_settings:
+                replaceFragment(new SettingsFragment());
+                return true;
+            case R.id.action_appInfo:
+                Toast.makeText(MainActivity.this, TOAST, Toast.LENGTH_SHORT).show();
+                replaceFragment(new AppInfoFragment());
+                return true;
+            case R.id.action_noteAdd:
+                replaceFragment(new AddNoteFragment());
+                return true;
+            case R.id.action_notesList:
+                replaceFragment(new NotesListFragment());
+                return true;
+        }
+        return false;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_settings:
+                replaceFragment(new SettingsFragment());
+                return true;
+            case R.id.action_search:
+                return true;
+            case R.id.action_noteAdd:
+                replaceFragment(new AddNoteFragment());
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem search = menu.findItem(R.id.action_search);
+        SearchView searchText = (SearchView) search.getActionView();
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+
+        return true;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.mainFragment, fragment);
+        fragmentTransaction.commit();
+    }
+
+
 }
